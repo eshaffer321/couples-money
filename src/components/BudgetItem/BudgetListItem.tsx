a93cd1c36cd6587712e55fc5ec7204e7a601978b
Erick Shaffer <erick.shaffer321@gmail.com>
2022-12-20 07:44:40 -0700

    WIP ripple effect when clicking budget item

--- src/components/BudgetItem/BudgetListItem.tsx
+++ src/components/BudgetItem/BudgetListItem.tsx
@@ -1,7 +1,8 @@
 import { BudgetItem } from "@prisma/client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { trpc } from "../../utils/trpc";
 import { ControlledInput } from "../ControlledInput";
+import { motion } from "framer-motion";
 
 interface Props {
   item: BudgetItem;
@@ -11,6 +12,9 @@ function ListItem(props: Props) {
   const budgetItem = props.item;
   const [budgeItemName, setBudgetItemName] = useState(budgetItem.name);
   const [budgeItemAmount, setBudgetItemAmount] = useState(budgetItem.amount);
+  const [isRippleVisible, setIsRippleVisible] = useState(false);
+  const [clickPosition, setClickPosition] = useState<{ x: number; y: number } | null>(null);
+  const [listItem, setListItem] = useState<HTMLLIElement | null>(null);
 
   const utils = trpc.useContext().budgetMonth;
   const mutation = trpc.budgetItem.update.useMutation({
@@ -40,8 +44,42 @@ function ListItem(props: Props) {
       amount: parseInt(event.target.value),
     });
   };
+
+  const openTransactionModal = (event: React.MouseEvent<HTMLLIElement>) => {
+    setClickPosition({ x: event.clientX, y: event.clientY });
+    setListItem(event.currentTarget);
+
+    console.log("open transaction modal");
+  };
+
+  useEffect(() => {
+    if (clickPosition && listItem) {
+      console.log("click position", clickPosition)
+      const ripple = document.createElement("div");
+      ripple.className = "ripple show";
+      ripple.style.top = `${clickPosition.y - listItem.offsetTop - listItem.offsetTop}px`;
+    ripple.style.left = `${
+      clickPosition.x - listItem.offsetLeft - listItem.offsetLeft
+    }px`;
+      ripple.style.width = "100%";
+      ripple.style.height = "100%";
+      listItem.appendChild(ripple);
+
+      setTimeout(() => {
+        ripple.classList.remove("show");
+        ripple.remove();
+      }, 250);
+    }
+  }, [clickPosition]);
+
   return (
-    <li key={budgetItem.id} className="px-6 py-4">
+    <motion.li
+      whileHover={{ scale: 1.01 }}
+      whileTap={{ scale: 0.99 }}
+      onClick={openTransactionModal}
+      key={budgetItem.id}
+      className="px-6 py-4 overflow-hidden"
+    >
       <div className="flex items-center justify-between">
         <span className="text-sm font-medium text-gray-900">
           <ControlledInput
@@ -57,7 +95,7 @@ function ListItem(props: Props) {
           />
         </span>
       </div>
-    </li>
+    </motion.li>
   );
 }
 
